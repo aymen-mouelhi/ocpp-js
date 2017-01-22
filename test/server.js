1e6c31ae01780f48d07425a2f4ae1e686caa928e
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-22 19:37:18 +0100

    rpc calls

--- test/server.js
+++ test/server.js
@@ -2,7 +2,6 @@ const CentralSystem = require('../entities/CentralSystem');
 
 var system = new CentralSystem(9000, 'websocket');
 
-
 setInterval(function(){
   var connection;
   if (system.getConnections()) {
