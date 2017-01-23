d486ac61ca7834ba506951710a65415327ce2167
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-24 00:54:33 +0100

    rpc calls: promises

--- handlers/bootNotification.js
+++ handlers/bootNotification.js
@@ -1,15 +1,29 @@
 const firebase = require('firebase');
+const Promise = require('promise');
+
+var config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+  };
+
+// TODO: handle firebase Authentication
+firebase.initializeApp(config);
 
 module.exports = {
   handle: function(data){
-    // TODO: save data
-
-    // Return Reponse
-    // status can be Rejected or Accepted
-    return {
-        status: 'Accepted',
-        currentTime: new Date().toISOString(),
-        heartbeatInterval: 1200
-      }
+    return new Promise(function(resolve, reject) {
+      return firebase.database().ref('/stations/' + data.iccid).set(data).then(function(){
+        // Return Reponse
+        // status can be Rejected or Accepted
+        return {
+            status: 'Accepted',
+            currentTime: new Date().toISOString(),
+            heartbeatInterval: 1200
+          }
+      });
+    });
   }
 }
