a4bbdcae07cbe751916c506d16956d8ab60752d5
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-02-04 22:54:56 +0100

    check station exists

--- handlers/bootNotification.js
+++ handlers/bootNotification.js
@@ -1,27 +1,58 @@
 const Promise = require('promise');
+const moment = require('moment');
 const DB = require('../db/index.js');
 var Storage = new DB(process.env.storage);
 
 module.exports = {
   handle: function(data){
     return new Promise(function(resolve, reject) {
-      // TODO: check that station doesn't exist
-      // TODO: maybe save into notification collection when a boot occures
-      Storage.save('station', data, function(err){
-        if(err){
-          resolve({
-              status: 'Rejected',
-              currentTime: new Date().toISOString(),
-              heartbeatInterval: 1200
-            });
+      // Create Notification
+      var message = data.chargeBoxIdentity + ' has just started';
+
+      var notification = {
+        text: message,
+        unread: true,
+        createdOn: moment().format()
+      }
+
+      Storage.findAll('station').then(function(stations){
+        var station = stations.filter(function(item){
+          return item.chargePointSerialNumber === data.chargePointSerialNumber;
+        });
+
+        if(station){
+          // Station already exists
+          Storage.save('notification', notification, function(err){
+            if(err){
+              reject(err);
+            }else{
+              resolve({});
+            }
+          });
         }else{
-          // Return Reponse
-          // status can be Rejected or Accepted
-          resolve({
-              status: 'Accepted',
-              currentTime: new Date().toISOString(),
-              heartbeatInterval: 1200
-            });
+          Storage.save('notification', notification, function(err){
+            if(err){
+              reject(err);
+            }else{
+              Storage.save('station', data, function(err){
+                if(err){
+                  resolve({
+                      status: 'Rejected',
+                      currentTime: new Date().toISOString(),
+                      heartbeatInterval: 1200
+                    });
+                }else{
+                  // Return Reponse
+                  // status can be Rejected or Accepted
+                  resolve({
+                      status: 'Accepted',
+                      currentTime: new Date().toISOString(),
+                      heartbeatInterval: 1200
+                    });
+                }
+              });
+            }
+          });
         }
       });
     });
